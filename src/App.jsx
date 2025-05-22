ddfde538e1bfb0e8d14d5fafbc00bab690eb4cb3
uttkarsh singh <uttkarshsingh450@gmail.com>
2025-05-22 09:56:38 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ lenis.on('scroll', (e) => {
 useEffect(()=>{
   let timeline1 = gsap.timeline({scrollTrigger:{
     trigger: "#about",
-    markers: true,  
+    // markers: true,  
     start: "15% 50%",
     end: "20% 55%",
     scrub: 1,
@@ -38,7 +38,7 @@ useEffect(()=>{
 
     let timeline2 = gsap.timeline({scrollTrigger:{
     trigger: "#skill",
-    markers: true,  
+    // markers: true,  
     start: "0% 50%",
     end: "8% 55%",
     scrub: 1,
