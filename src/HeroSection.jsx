f931201cde83ec061f3f67b0decc0e0b22de9c49
uttkarsh singh <uttkarshsingh450@gmail.com>
2025-07-25 15:09:23 +0530

    responsive-5

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -7,8 +7,8 @@ const HeroSection = ({ id }) => {
   return (
     <div id={id} className="w-screen md:h-full flex flex-col gap-10">
       <Navbar />
-      <div className="h-full w-full pt-20  flex flex-col  border-gray-300 justify-between gap-5  items-center">
-        <p className="bg-gray-50 p-1 pl-3 pr-3 rounded-full border-4 border-white flex items-center gap-2 font-light text-[10px] md:text-[15px] text-center">
+      <div className="h-full w-full pt-8 md:pt-20  flex flex-col  border-gray-300 justify-between gap-5  items-center">
+        <p className="bg-gray-50 p-1 pl-3 pr-3 rounded-full border-4 border-white flex items-center gap-2 font-light text-[12px] md:text-[15px] text-center">
           <PiHandWavingLight /> Hi, I'm Uttkarsh
         </p>
         <h1 className="text-[28px] md:text-[72px] text-center tracking-tight leading-[40px] md:leading-[72px] font-medium">
@@ -19,21 +19,21 @@ const HeroSection = ({ id }) => {
          design. I turn ideas into smooth, functional experiences
         </p>
         <div className="button-grp gap-2 md:gap-5 flex pt-2 md:pt-10">
-          <button className="border pl-3 md:pl-7 pr-3 md:pr-7 p-1 md:p-3 rounded-full bg-black text-white cursor-pointer hover:bg-[#292929] text-[9px] md:text-[20px]"
+          <button className="border pl-4 md:pl-7 pr-4 md:pr-7 p-2 md:p-3 rounded-full bg-black text-white cursor-pointer hover:bg-[#292929] text-[9px] md:text-[20px]"
  onClick={() => window.location.href = "mailto:uttkarshsingh450@gmail.com?subject=Collaboration Request"}>
             Let's Collaborate
           </button>
-          <button   onClick={() => navigate("/work")} className="border border-gray-400  pl-3 md:pl-6 pr-3 md:pr-6 p-1 md:p-3 rounded-full cursor-pointer text-[9px] md:text-[20px]">
+          <button   onClick={() => navigate("/work")} className="border border-gray-400  pl-4 md:pl-6 pr-4 md:pr-6 p-2 md:p-3 rounded-full cursor-pointer text-[9px] md:text-[20px]">
             See Work
           </button>
         </div>
       </div>
 
-      <div className="w-[70vw] overflow-hidden rounded-xl group mx-auto">
+      <div className="w-[83vw] md:w-[70vw] overflow-hidden rounded-xl group mx-auto">
         <img
           src="https://res.cloudinary.com/df134toxg/image/upload/v1750240013/uttkarsh-passport-photo_1_-modified_cfoy81.png"
           alt="Uttkarsh"
-          className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
+          className="md:w-full h-[60vw] md:h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
         />
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ import { MdOutlineDarkMode } from "react-icons/md";
 const Navbar = () => {
   return (
     <div className="w-full flex justify-between items-center h-[10vh] p-3 sm:p-5 sm:pr-20 pt-4 sm:pt-6 md:pt-10 sm:overflow-hidden md:overflow-hidden">
-      <h1 className="instrument-serif-regular text-[10px] sm:text-2xl leading-7 pl-1 sm:pl-10">
+      <h1 className="instrument-serif-regular text-[16px] sm:text-2xl leading-7 pl-1 sm:pl-10">
         @byuttkarsh
       </h1>
           <div className="button-grp gap-2 sm:gap-5 flex ">
-      <button className=" sm:p-2 pt-1 pb-1 pl-3 sm:pl-6 pr-3 sm:pr-6 border rounded-full border-dotted border-gray-500 cursor-pointer text-[11px] sm:text-[13px]"
+      <button className="border border-gray-400  pl-4 md:pl-6 pr-4 md:pr-6 p-2 md:p-3 rounded-full cursor-pointer text-[9px] md:text-[20px]"
       onClick={() =>
             window.open(
               "https://drive.google.com/file/d/1hBX85vX9Xa61N-9dNdOrrhXUDfwUP5p2/view",
@@ -18,7 +18,7 @@ const Navbar = () => {
       >RESUME</button>
       <button
       onClick={() => window.location.href = "mailto:uttkarshsingh450@gmail.com?subject=Collaboration Request"}
-      className="sm:p-2 pl-3 sm:pl-6 pr-3 sm:pr-6 pt-1 pb-1  border rounded-full border-dotted border-gray-500 cursor-pointer text-[11px] sm:text-[13px]">CONNECT</button>
+      className="border border-gray-400  pl-4 md:pl-6 pr-4 md:pr-6 p-2 md:p-3 rounded-full cursor-pointer text-[9px] md:text-[20px]">CONNECT</button>
     </div>
     </div>
   );
