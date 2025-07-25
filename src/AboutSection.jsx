793b50b06ac57e6eaf8fba649e21fd758cc5577e
uttkarsh singh <uttkarshsingh450@gmail.com>
2025-07-25 15:27:33 +0530

    responsive-7

--- src/AboutSection.jsx
+++ src/AboutSection.jsx
@@ -6,9 +6,9 @@ const AboutSection = ({ id }) => {
     <>
       <div
         id={id}
-        className="w-screen h-screen md:flex gap-6 sm:gap-10 md:gap-20 md:p-20 pt-28 md:pt-60 pl-5 md:pl-28 pr-5 md:pr-20"
+        className="w-screen h-screen md:flex gap-6 sm:gap-10 md:gap-20 md:p-20 pt-33 md:pt-60 pl-5 md:pl-28 pr-5 md:pr-20"
       >
-        <Header as="h1" className="text-[12px] md:text-[20px] w-[22vw]">
+        <Header as="h1" className="text-[20px] md:text-[20px] w-[35vw]">
           {"{ About Me }"}
         </Header>
         <p className="text-[16px] pt-5 md:text-[32px] tracking-normal">
